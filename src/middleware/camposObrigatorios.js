cfbe521d1cc2d605d21de665b18c87d9d188056a
Systemilc <isaac.systemilc@gmail.com>
2023-09-20 00:44:14 -0300

    Criado configuração para criação de documentação com Swagger e adicionado informações sobre rota Post

--- src/middleware/camposObrigatorios.js
+++ src/middleware/camposObrigatorios.js
@@ -1,8 +1,7 @@
-const todosOsCampos = (req, res, next)=> {
+const todosOsCampos = (req, res, next) => {
     const { modelo, marca, descricao, preco_aluguel, categoria, disponivel } = req.body
-    console.log(modelo, marca, descricao, preco_aluguel, categoria, disponivel)
-    if(!modelo || !marca || !descricao || !preco_aluguel || !categoria || !disponivel){
-       return res.json({error: "É necessário informar todos os campos para criar ou atualiuzar um veículo."})
+    if (!modelo || !marca || !descricao || !preco_aluguel || !categoria || !disponivel) {
+        return res.status(400).json({ error: "É necessário informar todos os campos para criar ou atualizar um veículo." })
     }
 
     next()
--- src/servidor.js
+++ src/servidor.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
